b3c86e97ddb9be3b5fb83f9314fe936556e4b501
Ridmi95 <yrridmi1995@gmail.com>
2019-08-03 11:23:42 +0530

    commit 2

---
+++ app/src/main/java/com/example/pushup/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.pushup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void TextUpload(){
+         int i = 1 +1;
+
+    }
+}
